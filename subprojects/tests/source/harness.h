fd0e92ed0b7525103332b06e76b08152dc67c753
Lorenzo Delgado <lnsdev@proton.me>
2025-12-30 20:41:01 +0100

    Revert "feat(tests): enhance test harness"
    This reverts commit b8c2c0f835dd17c68154f9f78a7b02660e52ddbc.

--- subprojects/tests/source/harness.h
+++ subprojects/tests/source/harness.h
@@ -2,20 +2,11 @@
 
 #include <stdint.h>
 #include <stdio.h>
-#include <threads.h>
-#include <inttypes.h>
-
-#include "nx_sync_oneshot.h"
 
 /**
  * @brief The result code for a test case.
  */
 typedef int32_t test_rc_t;
-
-/**
- * Test case function
- */
-typedef test_rc_t (*TestCaseFn)(void);
 
 /**
  * Test suite function
@@ -37,10 +28,6 @@ typedef void (*TestSuiteFn)(void);
  */
 #define TEST_TODO ((test_rc_t)-501)
 
-/**
- * @brief The failure code for a test case that was skipped.
- */
-#define TEST_SKIPPED ((test_rc_t)-502)
 
 /**
  * Test suite declaration.
@@ -51,23 +38,23 @@ typedef void (*TestSuiteFn)(void);
     printf("\n" CONSOLE_CYAN "TEST SUITE:" CONSOLE_RESET " " suite_name "\n\n");
 
 /**
- * @brief Arguments for a test case thread.
+ * Test case declaration.
+ *
+ * @param test_title The title of the test case.
+ * @param test_func The function to run for the test case.
  */
-typedef struct {
-    NxSyncOneshotSender* sender;
-    TestCaseFn func;
-} TestCaseThreadArgs;
-
-/**
- * @brief The entry point for a test case thread.
- * @param arg A pointer to the TestCaseThreadArgs struct.
- */
-static int test_case_thread_func(void* arg) {
-    TestCaseThreadArgs* args = (TestCaseThreadArgs*)arg;
-    test_rc_t rc = args->func();
-    __nx_sync_oneshot_send(args->sender, (void*)(intptr_t)rc);
-    return 0;
-}
+#define TEST_CASE(test_title, test_func) \
+    { \
+        printf(test_title ": "); \
+        test_rc_t test_res = test_func(); \
+        if (test_res == TEST_SUCCESS) { \
+            printf(CONSOLE_GREEN "OK" CONSOLE_RESET "\n"); \
+        } else if (test_res == TEST_TODO) { \
+            printf(CONSOLE_MAGENTA "TODO" CONSOLE_RESET "\n"); \
+        } else { \
+            printf(CONSOLE_RED "FAILED" CONSOLE_RESET " (%d)\n", test_res); \
+        } \
+    }
 
 /**
  * Skipped test case declaration.
@@ -78,43 +65,4 @@ static int test_case_thread_func(void* arg) {
 #define XTEST_CASE(test_title, test_func) \
     { \
         printf(test_title ": " CONSOLE_YELLOW "SKIPPED" CONSOLE_RESET "\n"); \
-    }
-
-/**
- * Test case declaration.
- *
- * @param test_title The title of the test case.
- * @param test_func The function to run for the test case.
- */
-#define TEST_CASE(test_title, test_func) \
-    { \
-        printf(test_title ": "); \
-        fflush(stdout); \
-        \
-        NxSyncOneshotSender* sender; \
-        NxSyncOneshotReceiver* receiver; \
-        __nx_sync_oneshot_create(&sender, &receiver); \
-        \
-        TestCaseThreadArgs args = { .sender = sender, .func = test_func }; \
-        thrd_t thread; \
-        if (thrd_create(&thread, test_case_thread_func, &args) != thrd_success) { \
-            printf(CONSOLE_RED "HARNESS_ERROR: thread_create failed" CONSOLE_RESET "\n"); \
-            __nx_sync_oneshot_sender_free(sender); \
-            __nx_sync_oneshot_receiver_free(receiver); \
-        } else { \
-            void* recv_value = NULL; \
-            if (__nx_sync_oneshot_recv(receiver, &recv_value) == 0) { \
-                test_rc_t test_res = (test_rc_t)(intptr_t)recv_value; \
-                if (test_res == TEST_SUCCESS) { \
-                    printf(CONSOLE_GREEN "OK" CONSOLE_RESET "\n"); \
-                } else if (test_res == TEST_TODO) { \
-                    printf(CONSOLE_MAGENTA "TODO" CONSOLE_RESET "\n"); \
-                } else { \
-                    printf(CONSOLE_RED "FAILED" CONSOLE_RESET " (%d)\n", test_res); \
-                } \
-            } else { \
-                printf(CONSOLE_RED "HARNESS_ERROR: recv failed" CONSOLE_RESET "\n"); \
-            } \
-            thrd_join(thread, NULL); \
-        } \
     }
